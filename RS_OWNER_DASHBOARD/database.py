c8c7b45892deacdbb6dd69e5fa4cd57bad1e16f4
ryadav18 <ryadav810232@gmail.com>
2026-07-02 10:46:12 +0530

    Update database.py

--- RS_OWNER_DASHBOARD/database.py
+++ RS_OWNER_DASHBOARD/database.py
@@ -1,1 +1,8 @@
+from supabase import create_client, Client
+from config import Config
 
+# Ensure configurations are validated before initializing client
+Config.validate()
+
+# Single point of initialization for the application
+supabase: Client = create_client(Config.SUPABASE_URL, Config.SUPABASE_KEY)
